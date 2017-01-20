c8363bbf4bfb050f3cd654683d3e6ef71bced097
Asya229 <seoasya@gmail.com>
2017-01-21 00:24:31 +0300

    remove created date from Mainpage + makemigration (может понадобится)

--- barmetro/models.py
+++ barmetro/models.py
@@ -6,8 +6,6 @@ from django.utils import timezone
 class Mainpage(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
 
