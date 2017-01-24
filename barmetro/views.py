d5c193bad961521bb8de0a8351640ecdfdc3dd1e
Asya229 <seoasya@gmail.com>
2017-01-24 22:59:23 +0300

    Создана переменая coords в views , положила все обьекты (20 шт тест) В шаблоне через цикл for вывела параметры обьекта (метро coord)

--- barmetro/views.py
+++ barmetro/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import CoordMetro
 
 def page_list(request):
-    return render(request, 'barmetro/page_list.html', {})
+    coords = CoordMetro.objects.all()[:20]
+    return render(request, 'barmetro/page_list.html', {'coords': coords})
+
