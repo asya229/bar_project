cf94a1814a6035abbf999399b9c86355fd8a2fd3
Asya229 <seoasya@gmail.com>
2017-01-26 00:48:39 +0300

    создана кликабельная страница с станциями метро

--- barmetro/urls.py
+++ barmetro/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.page_list, name='page_list'),
+    url(r'^station/(?P<pk>[0-9]+)/$', views.page_metro, name='page_metro')
 ]
--- barmetro/views.py
+++ barmetro/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import CoordMetro
 
 def page_list(request):
     coords = CoordMetro.objects.all()[:20]
     return render(request, 'barmetro/page_list.html', {'coords': coords})
 
+def page_metro(request, pk):
+    station = get_object_or_404(CoordMetro, pk=pk)
+    return render(request, 'barmetro/page_metro.html', {'station': station})
