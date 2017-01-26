dd7a7297497a8108b84e7d4819f2f5e8fa4bfc22
Asya229 <seoasya@gmail.com>
2017-01-26 23:41:08 +0300

    Добавлена страница в url page_bar Добавлена на главную страницу Бары, сделана views для бара Создан  шаблон бара В шаблон глав. страницы добалены бары

--- barmetro/views.py
+++ barmetro/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import CoordMetro
+from .models import CoordMetro,CoordBar
 
 def page_list(request):
-    coords = CoordMetro.objects.all()[:20]
-    return render(request, 'barmetro/page_list.html', {'coords': coords})
+    coords = CoordMetro.objects.all()[:30]
+    coords_for_bar = CoordBar.objects.all()[:20]
+    return render(request, 'barmetro/page_list.html', {'coords': coords, 'coords_for_bar': coords_for_bar})
 
 def page_metro(request, pk):
     station = get_object_or_404(CoordMetro, pk=pk)
     return render(request, 'barmetro/page_metro.html', {'station': station})
+
+def page_bar(request, pk):
+    bar = get_object_or_404(CoordBar, pk=pk)
+    return render(request, 'barmetro/page_bar.html', {'bar': bar})
+
+
